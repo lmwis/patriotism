16562dfeb223251de91b9ebb44ee6a4b49ac0698
W1948730080 <1948730080@qq.com>
2019-07-18 12:33:38 +0800

    fehead-initialize root

---
+++ fehead-initialize/src/main/java/com/fehead/InitializeApplication.java
@@ -0,0 +1,14 @@
+package com.fehead;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class InitializeApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

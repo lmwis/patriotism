7ead488ac4362c34eb3e0fa25972663023e67d00
lmwis <detectiveygn@163.com>
2019-08-11 20:07:41 +0800

    core data 1.1

---
+++ patriotism-core-data/src/main/java/com/fehead/controller/VideoDataController.java
@@ -0,0 +1,25 @@
+package com.fehead.controller;
+
+import com.fehead.response.CommonReturnType;
+import com.fehead.response.FeheadResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lmwis on 2019-08-11 19:48
+ */
+@RequestMapping("/data/video")
+@RestController
+public class VideoDataController {
+
+
+    @GetMapping("/lists")
+    public FeheadResponse videoLists(Page){
+
+
+
+        return CommonReturnType.create("nu");
+    }
+
+}
